3cd38c0d7726a86e5d75ed02dddce93274118240
Nazmi Anik <nazmianik@gmail.com>
2015-08-06 14:42:11 -0400

    Install, Unzip the file, Read and Subset the Data

--- plot2.R
+++ plot2.R
@@ -3,4 +3,17 @@ plot2 <- function() {
     # Exploratory Data Analysis
     # Course Project 1
     
+    # Download the file and extract the portion needed
+    
+    url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    download.file(url, destfile = ".\\data\\power.zip", mode = "wb")
+    file <- unzip(".\\data\\power.zip", exdir = ".\\data")
+    
+    # We will only be using data from the dates 2007-02-01 and 2007-02-02.
+    
+    ## Read all the data and subset
+    power <- read.table(file, header=TRUE, sep=";", na.strings="?", stringsAsFactors=FALSE)
+    power[,1] <- as.Date(power[,1],format='%d/%m/%Y')
+    power <- power[(power$Date=="2007/02/01") | (power$Date=="2007/02/02"),]
+    
 }
