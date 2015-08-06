5bfc331c67c6debc09af744a690ef5a3249b35bd
Nazmi Anik <nazmianik@gmail.com>
2015-08-06 12:55:40 -0400

    Add Plot 1 Script File
    Start working on the first plot

---
+++ plot1.R
@@ -0,0 +1,6 @@
+# Exploratory Data Analysis
+# Course Project 1
+
+# Download the file and extract the portion needed
+
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
