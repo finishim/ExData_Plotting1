30044cb8e8c5c1e3f56aa3c8495d9aa398e76093
Nazmi Anik <nazmianik@gmail.com>
2015-08-06 15:48:01 -0400

    Add Legend

--- plot3.R
+++ plot3.R
@@ -20,12 +20,18 @@ plot3 <- function() {
     power <- within(power, datetime <- strptime(paste(power$Date, power$Time), "%Y-%m-%d %H:%M:%S"))
     ### source: http://stackoverflow.com/questions/5250978/colclasses-date-and-time-read-csv
     
-    #Plot the graph
+    # Plot the graph
     plot(power$datetime, power$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
-    lines(powe$datetime, power$Sub_metering_2, type="l", col="red")
+    lines(power$datetime, power$Sub_metering_2, type="l", col="red")
+    lines(power$datetime, power$Sub_metering_3, type="l", col="blue")
+    ### source: http://stackoverflow.com/questions/2564258/plot-two-graphs-in-same-plot-in-r
+    
+    # Add Legend
+    legend(x="topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1, lwd=2)
+    ### source: http://www.inside-r.org/r-doc/graphics/legend
     
     # Save the plot to a file
-    dev.copy(device = png, filename = 'plot2.png', width = 480, height = 480)
+    dev.copy(device = png, filename = 'plot3.png', width = 480, height = 480)
     dev.off()
     ### source: http://stackoverflow.com/questions/7144118/how-to-save-a-plot-as-image-on-the-disk
     
