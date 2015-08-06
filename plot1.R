e410e9cbe5995cba8e0c8b3a3e4e1beb0136410f
Nazmi Anik <nazmianik@gmail.com>
2015-08-06 14:16:32 -0400

    Read and Subset Data

--- plot1.R
+++ plot1.R
@@ -5,10 +5,19 @@
 
 url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 download.file(url, destfile = ".\\data\\power.zip", mode = "wb")
-unzip(".\\data\\power.zip", exdir = ".\\data")
+file <- unzip(".\\data\\power.zip", exdir = ".\\data")
 
 
-## We will only be using data from the dates 2007-02-01 and 2007-02-02. 
+# We will only be using data from the dates 2007-02-01 and 2007-02-02. 
+
 ## Read the data from just those dates
+##library(sqldf)
+##power2 <- read.csv.sql(file, header=TRUE, sep=";", stringsAsFactors=FALSE, sql = 'select * from file where Date = "2007-02-01" or Date = "2007-02-02"')
+### source: http://stackoverflow.com/questions/3554572/read-csv-with-dates-and-numbers-with-r-statistical-software
+
+## Read all the data and subset
+power <- read.table(file, header=TRUE, sep=";", na.strings="?", stringsAsFactors=FALSE)
+power[,1] <- as.Date(power[,1],format='%d/%m/%Y')
+power <- power[(power$Date=="2007/02/01") | (power$Date=="2007/02/02"),]
 
 ## Convert Date and Time variables to Date/Time classes in R using the `strptime()` and `as.Date()`
