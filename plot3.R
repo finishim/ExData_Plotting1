aeab2c378e0658ec57703ca543be8884ee256e08
Nazmi Anik <nazmianik@gmail.com>
2015-08-07 14:25:52 -0400

    Open PNG Device First
    Fixes the legend margin issue

--- plot3.R
+++ plot3.R
@@ -21,17 +21,20 @@ plot3 <- function() {
     ### source: http://stackoverflow.com/questions/5250978/colclasses-date-and-time-read-csv
     
     # Plot the graph
+    
+    png(filename = 'plot3.png', width = 480, height = 480)
     plot(power$datetime, power$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
     lines(power$datetime, power$Sub_metering_2, type="l", col="red")
     lines(power$datetime, power$Sub_metering_3, type="l", col="blue")
     ### source: http://stackoverflow.com/questions/2564258/plot-two-graphs-in-same-plot-in-r
     
     # Add Legend
+    
     legend(x="topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1, lwd=2)
     ### source: http://www.inside-r.org/r-doc/graphics/legend
     
     # Save the plot to a file
-    dev.copy(device = png, filename = 'plot3.png', width = 480, height = 480)
+    #dev.copy(device = png, filename = 'plot3.png', width = 480, height = 480)
     dev.off()
     ### source: http://stackoverflow.com/questions/7144118/how-to-save-a-plot-as-image-on-the-disk
     
