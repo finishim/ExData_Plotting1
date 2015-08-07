41d7666f9eb3a901d98623bca33ef02d1865d48b
Nazmi Anik <nazmianik@gmail.com>
2015-08-07 14:28:55 -0400

    Fix Comment Spacing for Readibility

--- plot2.R
+++ plot2.R
@@ -3,13 +3,17 @@ plot2 <- function() {
     # Exploratory Data Analysis
     # Course Project 1
     
+    
     # Download the file and extract the portion needed
+    
     
     url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
     download.file(url, destfile = ".\\data\\power.zip", mode = "wb")
     file <- unzip(".\\data\\power.zip", exdir = ".\\data")
     
+    
     # We will only be using data from the dates 2007-02-01 and 2007-02-02.
+    
     
     ## Read all the data and subset
     power <- read.table(file, header=TRUE, sep=";", na.strings="?", stringsAsFactors=FALSE)
@@ -20,10 +24,14 @@ plot2 <- function() {
     power <- within(power, datetime <- strptime(paste(power$Date, power$Time), "%Y-%m-%d %H:%M:%S"))
     ### source: http://stackoverflow.com/questions/5250978/colclasses-date-and-time-read-csv
     
+    
     #Plot the graph
     plot(power$datetime, power$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
     
+    
     # Save the plot to a file
+    
+    
     dev.copy(device = png, filename = 'plot2.png', width = 480, height = 480)
     dev.off()
     ### source: http://stackoverflow.com/questions/7144118/how-to-save-a-plot-as-image-on-the-disk
